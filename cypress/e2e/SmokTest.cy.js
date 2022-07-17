3b962ebac4b3982c25e9d20cef18e5a2c0e841d0
MichalFornal <michal.fornal@livekid.com>
2022-07-17 19:16:29 +0200

    Update test

--- cypress/e2e/SmokTest.cy.js
+++ cypress/e2e/SmokTest.cy.js
@@ -36,7 +36,7 @@ describe('sprawdzenie obecnosci', () => {
     cy.get('#datepicker-button').click();
     cy.get('#calendar-cell-22').click();
     cy.get('#datepicker-submit').click();
-    cy.wait(2000)
+    cy.wait(5000)
     cy.get('.list-content>div>span>div').find("#checkbox-default").each((elem)=>{
     console.log('dsds',elem)
     elem.trigger('click')
@@ -44,44 +44,87 @@ describe('sprawdzenie obecnosci', () => {
     cy.get(':nth-child(1) > .regular-list-actions > .more-icon > .material-icons').click();
     
   })
-  it("Kalendarzowy",() => {
+})
+  describe('Kalendarzowy', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  
+  it("Kalendarzowy open",() => {
     cy.get(':nth-child(3) > .navigation-cell').click();
     cy.wait(1000)
+    })
   })
-  it("Ogłoszenia",() => {
+  describe('Ogłoszenia', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Ogłoszenia open",() => {
     cy.get(':nth-child(4) > .navigation-cell').click();
     cy.wait(1000)
+    })
   })
-  it("Plan Zajęć",() => {
+  describe('Plan Zajęć', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Plan Zajęć open",() => {
     cy.get(':nth-child(5) > .navigation-cell').click();
     cy.wait(1000)
+    })
   })
-  it("Plan Zajęć",() => {
-    cy.get(':nth-child(5) > .navigation-cell').click();
-    cy.wait(1000)
-  })
-  it("Galeria Zdjęć",() => {
+  describe('Galeria zdjęć', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Galeria Zdjęć open",() => {
     cy.get(':nth-child(6) > .navigation-cell').click();
     cy.wait(1000)
+    })
   })
-  it("Jadłospis",() => {
+  describe('Jadłospis', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Jadłospis open",() => {
     cy.get(':nth-child(7) > .navigation-cell').click();
     cy.wait(1000)
+    })
   })
-  it("Dysk",() => {
+  describe('Dysk', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Dysk open",() => {
     cy.get(':nth-child(8) > .navigation-cell').click();
     cy.wait(1000)
+    })
   })
-  it("E-dziennik",() => {
+  describe('E-dziennik', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("E-dziennik open",() => {
     cy.get(':nth-child(9) > .navigation-cell').click();
     cy.wait(1000)
+    })
   })
-  it("Ankieta",() => {
+  describe('Ankieta', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Ankieta open",() => {
     cy.get(':nth-child(10) > .navigation-cell').click();
     cy.wait(1000)
+    })
   })
-  it("Ewaluacja",() => {
+  describe('Ewaluacja', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Ewaluacja open",() => {
     cy.get(':nth-child(11) > .navigation-cell').click();
     cy.wait(1000)
-  })
-})
+    })
+  }) 
+
