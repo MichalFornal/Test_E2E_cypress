b8f982bcd0bb04127ce804a2d44c4d1b2accb580
MichalFornal <michal.fornal@livekid.com>
2022-07-19 17:02:05 +0200

    update test

--- cypress/e2e/SmokTest.cy.js
+++ cypress/e2e/SmokTest.cy.js
@@ -42,10 +42,24 @@ describe('sprawdzenie obecnosci', () => {
     elem.trigger('click')
 
     })
-    cy.get(':nth-child(1) > .regular-list-actions > .more-icon > .material-icons').click();
-    
+  
   })
+  it("Trzy kropki",() => {
+    cy.get(':nth-child(1) > .regular-list-actions > .more-icon > .material-icons').click();
+    cy.wait(2000)
+    })
+    it("Powiadomienia",() => {
+      cy.get(':nth-child(1) > :nth-child(5) > .list-selector-container > .selector-display').click();
+      cy.get('#toggleCreator').click();
+      cy.get(':nth-child(6) > :nth-child(4) > #checkbox-default').click();
+      cy.get(':nth-child(5) > :nth-child(4) > #checkbox-default').click();
+      cy.get(':nth-child(4) > :nth-child(4) > #checkbox-default').click();
+      cy.wait(1000)
+      cy.get('#addNote').click();
+      cy.wait(2000)
+      })
 })
+
   describe('Kalendarzowy', () => {
     beforeEach(()=>{
       cy.viewport(1920, 1080) 
@@ -62,6 +76,8 @@ describe('sprawdzenie obecnosci', () => {
     })
   it("Ogłoszenia open",() => {
     cy.get(':nth-child(4) > .navigation-cell').click();
+    cy.wait(3000)
+    cy.reload()
     cy.wait(2000)
     })
   })
@@ -71,7 +87,7 @@ describe('sprawdzenie obecnosci', () => {
     })
   it("Plan Zajęć open",() => {
     cy.get(':nth-child(5) > .navigation-cell').click();
-    cy.wait(1000)
+    cy.wait(2000)
     })
   })
   describe('Galeria zdjęć', () => {
@@ -117,6 +133,7 @@ describe('sprawdzenie obecnosci', () => {
   it("Ankieta open",() => {
     cy.get(':nth-child(10) > .navigation-cell').click();
     cy.wait(1000)
+    
     })
   })
   describe('Ewaluacja', () => {
@@ -149,15 +166,15 @@ describe('sprawdzenie obecnosci', () => {
     cy.get('[href="/school/payments"] > .material-icons').click();
     cy.wait(2000)
     
-    // describe('Raporty', () => {
-    //   beforeEach(()=>{
-    //     cy.viewport(1920, 1080)
-    //     cy.wait(2000) 
-    //   })
-    // it("Raporty open",() => {
-    //   cy.get(':nth-child(2) > .navigation-cell').click();
-    //   cy.wait(5000)
-    //   })
+    describe('Raporty', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080)
+        cy.wait(2000) 
+      })
+    it("Raporty open",() => {
+      cy.get(':nth-child(2) > .navigation-cell').click();
+      cy.wait(2000)
+      })
   })  
     }) 
     describe('Wpłaty', () => {
@@ -169,53 +186,57 @@ describe('sprawdzenie obecnosci', () => {
       cy.wait(2000)
       })
     }) 
-    // describe('Faktury VAT', () => {
-    //   beforeEach(()=>{
-    //     cy.viewport(1920, 1080) 
-    //   })
-    // it("Faktury VAT open",() => {
-    //   cy.get(':nth-child(4) > .navigation-cell').click();
-    //   cy.wait(2000)
-    //   })
-    // }) 
-  //   describe('Rachunki', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Rachunki open",() => {
-  //     cy.get(':nth-child(1) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })  
-  //    })  
-  // }) 
-  // describe('Personel', () => {
-  //   beforeEach(()=>{
-  //     cy.viewport(1920, 1080) 
-  //   })
-  // it("Personel open",() => {
-  //   cy.get('[href="/school/employee"] > .material-icons',{ force: true }).click();
-  //   cy.wait(4000)
-  //   })
-  //   describe('Rejestr', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Rejestr open",() => {
-  //     cy.get(':nth-child(2) > .navigation-cell > .header-icon > .material-icons').click();
-  //     cy.wait(2000)
-  //     })
-  //   })
-  //   describe('Zadania personelu', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Zadania personelu open",() => {
-  //     cy.get(':nth-child(3) > .navigation-cell > .header-icon > .material-icons').click();
-  //     cy.wait(2000)
-  //     })
-  //   })
-  // nowy kod niepewny
-  //  }) 
+    describe('Faktury VAT', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Faktury VAT open",() => {
+      cy.get(':nth-child(4) > .navigation-cell').click();
+      cy.wait(4000)
+      cy.reload()
+      cy.wait(2000)
+      })
+    }) 
+    describe('Rachunki', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Rachunki open",() => {
+      cy.get(':nth-child(1) > .navigation-cell').click();
+      cy.wait(2000)
+      })  
+     })  
+  }) 
+  describe('Personel', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Personel open",() => {
+    cy.get('[href="/school/employee"] > .material-icons',{ force: true }).click();
+    cy.wait(4000)
+    cy.reload()
+    cy.wait(2000)
+    })
+    describe('Rejestr', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Rejestr open",() => {
+      cy.get(':nth-child(2) > .navigation-cell > .header-icon > .material-icons').click();
+      cy.wait(2000)
+      })
+    })
+    describe('Zadania personelu', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Zadania personelu open",() => {
+      cy.get(':nth-child(3) > .navigation-cell > .header-icon > .material-icons').click();
+      cy.wait(2000)
+      })
+    })
+  
+   }) 
   describe('E-zajęcia', () => {
     beforeEach(()=>{
       cy.viewport(1920, 1080) 
@@ -285,96 +306,104 @@ describe('sprawdzenie obecnosci', () => {
     it("Wiadomosci Posilki",() => {
       cy.get(':nth-child(1) > .navigation-cell').click();
       cy.wait(2000)
+      cy.reload()
+      cy.wait(2000)
       })
     }) 
   }) 
-  // describe('Ustawienia', () => {
-  //   beforeEach(()=>{
-  //     cy.viewport(1920, 1080) 
-  //   })
-  // it("Ustawienia open",() => {
-  //   cy.get('[href="/school/settings"] > .material-icons').click();
-  //   cy.wait(2000)
-  //   })
-  //   describe('Dzieci', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Dzieci open",() => {
-  //     cy.get(':nth-child(2) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })
-  //   }) 
-  //   describe('Grupy', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Grupy open",() => {
-  //     cy.get(':nth-child(3) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })
-  //   })
-  //   describe('Nauczyciele', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Nauczyciele open",() => {
-  //     cy.get(':nth-child(4) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })
-  //   }) 
-  //   describe('Placówka', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Placówka open",() => {
-  //     cy.get(':nth-child(5) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })
-  //   }) 
-  //   describe('Rekrutacja', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Rekrutacja open",() => {
-  //     cy.get(':nth-child(6) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })
-  //   }) 
-  //   describe('Uprawnienia', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Uprawnienia open",() => {
-  //     cy.get(':nth-child(7) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })
-  //   }) 
-  //   describe('Kalendarz pracy', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Kalendarz pracy open",() => {
-  //     cy.get(':nth-child(8) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })
-  //   }) 
-  //   describe('Panel Glowny parcy', () => {
-  //     beforeEach(()=>{
-  //       cy.viewport(1920, 1080) 
-  //     })
-  //   it("Panel Glowny open",() => {
-  //     cy.get(':nth-child(1) > .navigation-cell').click();
-  //     cy.wait(2000)
-  //     })
-  //   }) 
-  // }) 
+  describe('Ustawienia', () => {
+    beforeEach(()=>{
+      cy.viewport(1920, 1080) 
+    })
+  it("Ustawienia open",() => {
+    cy.get('[href="/school/settings"] > .material-icons').click();
+    cy.wait(2000)
+    cy.reload()
+    cy.wait(2000)
+    })
+    describe('Dzieci', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Dzieci open",() => {
+      cy.get(':nth-child(2) > .navigation-cell').click();
+      cy.wait(2000)
+      })
+    }) 
+    describe('Grupy', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Grupy open",() => {
+      cy.get(':nth-child(3) > .navigation-cell').click();
+      cy.wait(3000)
+      cy.reload()
+      cy.wait(2000)
+      })
+    })
+    describe('Nauczyciele', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Nauczyciele open",() => {
+      cy.get(':nth-child(4) > .navigation-cell').click();
+      cy.wait(2000)
+      })
+    }) 
+    describe('Placówka', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Placówka open",() => {
+      cy.get(':nth-child(5) > .navigation-cell').click();
+      cy.wait(2000)
+      })
+    }) 
+    describe('Rekrutacja', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Rekrutacja open",() => {
+      cy.get(':nth-child(6) > .navigation-cell').click();
+      cy.wait(2000)
+      })
+    }) 
+    describe('Uprawnienia', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Uprawnienia open",() => {
+      cy.get(':nth-child(7) > .navigation-cell').click();
+      cy.wait(2000)
+      })
+    }) 
+    describe('Kalendarz pracy', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Kalendarz pracy open",() => {
+      cy.get(':nth-child(8) > .navigation-cell').click();
+      cy.wait(2000)
+      })
+    }) 
+    describe('Panel Glowny parcy', () => {
+      beforeEach(()=>{
+        cy.viewport(1920, 1080) 
+      })
+    it("Panel Glowny open",() => {
+      cy.get(':nth-child(1) > .navigation-cell').click();
+      cy.wait(2000)
+      cy.reload()
+      cy.wait(2000)
+      })
+    }) 
+  }) 
   // describe('Konto', () => {
   //   beforeEach(()=>{
   //     cy.viewport(1920, 1080) 
   //   })
   // it("Konto open",() => {
-  //   cy.get('[href="/school/account"] > .sidebar-wrap-row-background',{force: true}).click();
+  //   cy.get('[href="/school/account"] > .sidebar-wrap-row-background').click();
   //   cy.wait(2000)
   //   })
   //   describe('Płatności', () => {
